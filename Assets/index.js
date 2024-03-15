9ba866328f61133a885f1c48934406f3a58a25c2
SamsonGT <96789962+SamsonGulma@users.noreply.github.com>
2024-03-15 05:48:51 -0700

    Uploading the Client Side Dev.
    -Uploading the Client Side Dev. directly from my local PC.
    -The Front-End Dev. Service was built using Just the mark-up language, just using the style-sh*t , Ahh I mean the style sheet😜 and very small code snippets of JS.
    -No Framework at all.

---
+++ Assets/index.js
@@ -0,0 +1,42 @@
+// let passStrength = document.getElementById("passStrength");
+// let symbols = "!@#$%^&*(){}[]/?><.,";
+// let password = document.getElementById("password");
+// let passwordScore = 0;
+
+
+// for (var i = 0; i < symbols.length; i++) {
+//     if (symbols.indexOf(password.charAt(i)) > -1) {
+//         passwordScore += 20;
+//     }
+// }
+
+// if (/[a-z]/.test(password)) {
+//     passwordScore += 20;
+// }
+
+// if (/[\d]/.test(password)) {
+//     passwordScore += 20;
+// }
+
+// var strength = "";
+// var backgroundColor = "";
+
+// if (passwordScore >= 100) {
+//     strength = "Strong";
+//     backgroundColor = "green"
+// }
+// if (passwordScore >= 80) {
+//     strength = "Medium";
+//     backgroundColor = "khaki"
+// }
+// if (passwordScore >= 60) {
+//     strength = "Weak";
+//     backgroundColor = "maroon"
+// }
+// if (passwordScore >= 40) {
+//     strength = "Very Weak";
+//     backgroundColor = "red"
+// }
+
+// document.getElementById("passStrength").innerHTML = strength;
+// document.getElementById("passStrength").style.backgroundColor = backgroundColor;
